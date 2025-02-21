06ae26793c5a1545905ce9f194bfd5bef60bfb0c
Harshad1551 <harshadbhavake77@gmail.com>
2025-02-21 17:04:44 +0530

    Preloader Updated

---
+++ src/components/LandingPage/Pre-loader.jsx
@@ -0,0 +1,10 @@
+import Fish from "../Assets/icons8-fish-50 1.jpg"
+function Preorder(){
+    return(
+        <div className="main-container">
+            <img src=""></img>
+            <h1>FishTankFR</h1>
+        </div>
+    );
+}
+export default Preorder
