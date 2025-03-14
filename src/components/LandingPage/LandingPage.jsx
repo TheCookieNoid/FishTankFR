464a4799a08c27eeb7549e8f8a9a85aa989a5853
Harshad1551 <harshadbhavake77@gmail.com>
2025-03-14 20:24:55 +0530

    Wrong renaming

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,20 @@
+import Fish from '../assets/icons-fish.jpg'
+
+function Preorder(){
+    return(
+        <div className="main-container">
+            <div className='sub-container1'>
+                <div className='top-elements'>
+            <img src={Fish}  alt="hello"></img>
+            <h2>0-100</h2>
+            </div>
+            <h1>FishTankFR</h1>
+            </div>
+            <div className='sub-container2'>
+            <p>We are a platform that helps you fund your ideas and bring them to life.</p>
+            <p className='copyright'>© 2025</p>
+            </div>
+        </div>
+    );
+}
+export default Preorder
