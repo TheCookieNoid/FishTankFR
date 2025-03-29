e816736c41884f3262d7d19c70e188bc2485af57
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-03-29 21:48:29 +0530

    Checkout done!

--- src/components/Pages/Checkout.jsx
+++ src/components/Pages/Checkout.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import fishLogoWhite from '../assets/icon-fish-transpernt.png'
 import '../CSS/Checkout.css'
+import MainImage from '../assets/Random-1.png'
+import UserPhoto from '../assets/Random.png'
 
 function Checkout(){
     document.body.style.backgroundColor = "black";
@@ -19,6 +21,33 @@ function Checkout(){
                 <li><a style={{color: 'white'}} href="">EXPLORE</a></li>
             </ul>
         </div>
+
+        <div className="project-details">
+            <img src={MainImage} id="project-title"/>
+            <div className="user-details">
+                <h1>RANDOM-1</h1>
+                <img src={UserPhoto} />
+                <h2>USER-1</h2>
+            </div>
+        </div>
+
+        <div className="message-container">
+            <p>Your pledge will support an ambitious creative project that has yet to be developed. There's a risk that, despite a creator's efforts the project may fail, We urge you to consider this risk prior to pledging. Fishtankfr is not responsible for fraud or refunds.</p>
+        </div>
+
+        <div className="price-button-container">
+            <h1 style={{fontWeight: '100'}}>PLEDGE AMOUNT:</h1>
+            <button>100</button>
+            <button>500</button>
+            <button>1000</button>
+            <button>2500</button>
+        </div>
+
+        <div className="checkout-container">
+            <p>Total : ₹</p>
+            <button>PLEDGE!</button>
+        </div>
+        
 
         </div>
     )
